192826fb43d2024b54f7451d03023aadc07b5679
Reid Zeng <zwc1986@126.com>
2016-09-14 14:23:45 +0800

    Add jsong printing.

--- src/main/java/com/yit/product/service/TestAction.java
+++ src/main/java/com/yit/product/service/TestAction.java
@@ -1,14 +1,39 @@
 package com.yit.product.service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import com.yit.common.entity.KeyValue;
+import com.yit.common.utils.HttpRequester;
+import com.yit.common.utils.RetryUtil;
 import com.yit.fcategory.api.FCategoryService;
 import com.yit.fcategory.entity.FCategoryInfo;
+import com.yit.file.api.FileService;
 import com.yit.keyvalue.api.KeyValueService;
+import com.yit.mail.api.EmailService;
+import com.yit.mail.entity.MailInfo;
+import com.yit.order.api.OrderService;
+import com.yit.order.entity.OrderBriefInfo;
+import com.yit.product.api.BrandService;
 import com.yit.product.api.CategoryService;
 import com.yit.product.api.ProductService;
+import com.yit.product.entity.Attribute;
+import com.yit.product.entity.CategoryInfo;
+import com.yit.product.entity.Product;
 import com.yit.vendor.api.VendorService;
 import com.yit.vendor.entity.Vendor;
+import com.yit.vendor.entity.VendorRunLog;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Resource;
+import javax.xml.ws.Provider;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.function.Function;
 
 
 /**
@@ -35,31 +60,36 @@ public class TestAction {
     @Resource
     private KeyValueService keyValueService;
 
+    @Resource
+    private FileService fileService;
+
+    @Autowired
+    private BrandService brandService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private EmailService emailService;
+
     public void start() throws Exception {
         System.out.println("=========start========");
+        long ms = new Date().getTime();
 
-        test2();
+        Product p = productService.getProductFromMagentoById(4247);
+        print(p);
 
-        //String value = keyValueService.get("");
-        //System.out.println(value);
-
+        ms = new Date().getTime() - ms;
+        System.out.println("====================");
+        System.out.printf("Time: %d ms.\n", (Long)ms);
         System.out.println("=========end========");
         System.exit(0);
     }
 
-    public void test1() {
-        while (true) {
-            int id = vendorService.createVendor(new Vendor());
-            Vendor v = vendorService.getVendorById(id);
-            vendorService.updateVendor(v);
-            System.out.println(v);
-        }
-    }
-
-    public void test2() {
-        FCategoryInfo item = fcategoryService.getFCategoryInfo(1, 1);
-
-        System.out.println(item);
+    public void print(Object obj) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String prettyJsonString = gson.toJson(obj);
+        System.out.println(prettyJsonString);
     }
 
 }
